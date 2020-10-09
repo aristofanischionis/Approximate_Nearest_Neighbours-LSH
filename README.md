f85bcf0dea6784a2a394e00d625e9ea9c1cd155e
Aristofanis Chionis <aristofanis.chionis@cern.ch>
2020-10-09 18:37:41 +0300

    Pass the met to the parent function

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Search_Cluster_Vectors_CPP
 University Assignment: Implement LSH algorithm for d-diamensonal vectors in C++
+
+We can run it like this for now:
+`g++ -o a.out main.cpp modules/manhattan-distance.cpp modules/handle-input.cpp modules/read-file.cpp && ./a.out -d "../Datasets/dataset" -q "rr" -k 1 -L 1 -o "file" -N 2 -R 22`
+
+_Provided that we have the dataset in the folder ./Datasets/dataset_
--- src/modules/read-file.cpp
+++ src/modules/read-file.cpp
@@ -1,18 +1,20 @@
 // Function to read binary files
 #include <iostream>
 #include <fstream>
-// #include <sstream>
+#include <vector> 
 #include <string>
 
 #include "../headers/handle-input.hpp"
 using namespace std;
 
 // read the first lines of the file, that contain the metadata
-void getMeta(ifstream *file) {
-    uint32_t magic_number;
-    uint32_t number_of_images;
-    uint32_t number_of_rows;
-    uint32_t number_of_columns;
+void getMeta(
+    ifstream *file, 
+    uint32_t &magic_number, 
+    uint32_t &number_of_images, 
+    uint32_t &number_of_rows, 
+    uint32_t &number_of_columns
+    ) {
     // read them from file
     (*file).read(reinterpret_cast<char *>(&magic_number), sizeof(magic_number));
     (*file).read(reinterpret_cast<char *>(&number_of_images), sizeof(number_of_images));
@@ -23,44 +25,45 @@ void getMeta(ifstream *file) {
     number_of_images = __builtin_bswap32(number_of_images);
     number_of_rows = __builtin_bswap32(number_of_rows);
     number_of_columns = __builtin_bswap32(number_of_columns);
-    // 
-    cout << "magic_number is: " << magic_number << endl;
-    cout << "number_of_images is: " << number_of_images << endl;
-    cout << "number_of_rows is: " << number_of_rows << endl;
-    cout << "number_of_columns is: " << number_of_columns << endl;
     return;
 }
 
 // read 784 bytes from file which are equal to an image
 // return vector!
 // each byte is a component and has a value between 0-255
-int readImage(char* memblock) {
+// the vector stores 'unsigned char', because it's the most efficient
+// unsigned char is a number between 0-255
+// alternatively we could use uint8_t (but needs include of a library)
+int readImage() {
     return 0;
 }
 
 // handling the input file
 void readFile(const std::string& filename, int file_type) {
     // Checked and I receive the filename string properly
-    cout << "my file name is: " << filename << endl;
-    // check if filename exists
-    // ios::ate positions the pointer in the end of the file
-    // this way using: file.seekg(0, ios::beg); will give us the size of the file
     ifstream file;
-    file.open(filename, ios::in|ios::binary|ios::ate);
-    streampos size;
-    // char* memblock;
+    uint32_t magic_number = 0;
+    uint32_t number_of_images = 0;
+    uint32_t number_of_rows = 0;
+    uint32_t number_of_columns = 0;
+
+    cout << "my file name is: " << filename << endl;
+
+    file.open(filename, ios::in|ios::binary);
     // open file to start reading
     if (file.is_open()) {
-        size = file.tellg();
         // memblock = new char[size];
         file.seekg(0, ios::beg);
-        cout << "the entire file content is in memory: " << size << endl;
         // read the data according to file_type
         switch (file_type){
             case INPUT_FILE:
-                // cout << "Hello Im in getme" << memblock << endl;
-                getMeta(&file);
+                getMeta(&file, magic_number, number_of_images, number_of_rows, number_of_columns);
+                cout << "magic_number is: " << magic_number << endl;
+                cout << "number_of_images is: " << number_of_images << endl;
+                cout << "number_of_rows is: " << number_of_rows << endl;
+                cout << "number_of_columns is: " << number_of_columns << endl;
                 // loop over all images to read them
+
                 break;
             case QUERY_FILE:
                 break;
@@ -69,7 +72,6 @@ void readFile(const std::string& filename, int file_type) {
                 break;
         }
         file.close();
-        // delete[] memblock;
     }
     else cout << "Unable to open file" << endl;
   return;
