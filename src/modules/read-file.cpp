a167a5941a3ef3b43558a4e2d6d2407e23c7937c
Stefanos Dianellos <stefanosdianellos@gmail.com>
2020-10-10 15:01:13 +0300

    Fix the bugs

--- src/modules/read-file.cpp
+++ src/modules/read-file.cpp
@@ -36,16 +36,16 @@ void getMeta(
 // alternatively we could use uint8_t (but needs include of a library)
 // BE CAREFUL WE PROBABLY NEED TO DO SOMETHING WITH THIS 28*28 
 // I'M NOT SURE HOW WE ARE SUPPOSED TO CREATE THE IMAGE pff
-unsigned char* readImage(ifstream *file, unsigned int size) {
+unsigned char* readImage(ifstream *file, uint64_t size) {
     unsigned char* image = new unsigned char[size];
 
     unsigned char byte;
     // read one-by-one 784 (28*28) unsigned bytes
-    for (int i = 0; i < size; i++){
+    for (uint64_t i = 0; i < size; i++){
         (*file).read(reinterpret_cast<char*>(&byte), 1);
         // with the following cout we see that most of the bytes have the value of 0
         // but some of them have values like 15, 235, 204, 124, etc
-        cout << "byte is: " << static_cast<unsigned>(byte) << endl;
+        // cout << "byte is: " << static_cast<unsigned>(byte) << endl;
         image[i] = byte;
     }
     return image;
@@ -64,13 +64,14 @@ void readFile(const std::string& filename, int file_type) {
         // read the data according to file_type
         switch (file_type){
             case INPUT_FILE:
+            {
                 // an array of vectors 
                 unsigned char** all_images;
                 uint32_t magic_number = 0;
                 uint32_t number_of_images = 0;
                 uint32_t number_of_rows = 0;
                 uint32_t number_of_columns = 0;
-                unsigned int size = 0;
+                uint64_t size = 0;
                 getMeta(&file, magic_number, number_of_images, number_of_rows, number_of_columns);
                 cout << "magic_number is: " << magic_number << endl;
                 cout << "number_of_images is: " << number_of_images << endl;
@@ -80,16 +81,17 @@ void readFile(const std::string& filename, int file_type) {
                 // initialize an array of vector items (all_images)
                 all_images = new unsigned char*[number_of_images];
                 // loop over all images to read them
-                for (int i = 0; i < number_of_images; i++){
+                for (uint32_t i = 0; i < number_of_images; i++){
                     all_images[i] = new unsigned char[size];
                     all_images[i] = readImage(&file, size);
                 }
                 // free-up memory space
-                for (int i = 0; i < number_of_images; i++){
+                for (uint32_t i = 0; i < number_of_images; i++){
                     delete[] all_images[i];
                 }
                 delete[] all_images;
                 break;
+            }
             case QUERY_FILE:
                 break;
             default:
