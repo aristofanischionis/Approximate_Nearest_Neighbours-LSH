d93be070b8121a93c1c2e505280854d7655257ad
Aristofanis Chionis <aristofanis.chionis@cern.ch>
2020-10-27 18:54:05 +0200

    WIP: kmeansPP in progress, NOT WORKING YET

--- makefile
+++ makefile
@@ -4,6 +4,9 @@ LSH_MODULES = src/modules/
 CUBE_PATH = src/headers/cube/
 CUBE_MODULES = src/modules/cube/
 
+KMEANSPP_PATH = src/headers/kmeansPP/
+KMEANSPP_MODULES = src/modules/kmeansPP/
+
 COMMONOBJS = manhattan-hashing.o manhattan-distance.o modulo.o handle-input.o read-file.o hashtable.o
 
 HEADER = $(LSH_HEADERS)handle-input.hpp $(LSH_HEADERS)distances.hpp $(LSH_HEADERS)manhattan-hashing.hpp $(LSH_HEADERS)modulo.hpp $(LSH_HEADERS)hashtable.hpp $(LSH_HEADERS)search.hpp $(LSH_HEADERS)common.hpp
@@ -14,7 +17,8 @@ HEADERCUBE = $(CUBE_PATH)handle-cube-input.hpp $(CUBE_PATH)projection.hpp $(CUBE
 SOURCECUBE = src/cube.cpp $(CUBE_MODULES)handle-cube-input.cpp $(CUBE_MODULES)read-cube-file.cpp $(CUBE_MODULES)projection.cpp
 OBJCUBE = cube.o handle-cube-input.o projection.o read-cube-file.o $(COMMONOBJS)
 
-OBJCLUSTER = cluster.o handle-cube-input.o projection.o read-cube-file.o search.o $(COMMONOBJS)
+# add the rest of obj files here
+OBJCLUSTER = cluster.o 
 
 CXX = g++
 FLAGS = -std=c++11 -O3 -g -c -Wall
@@ -39,6 +43,7 @@ lsh.o : $(SOURCE) $(HEADER)
 cube.o : $(SOURCECUBE) $(HEADERCUBE)
 	$(CXX) $(FLAGS) $(SOURCECUBE)
 
+# FOR CLUSTER
 # $(HEADER) $(CUBE_PATH)handle-cube-input.hpp $(CUBE_PATH)projection.hpp $(CUBE_PATH)read-cube-file.hpp
 # $(HEADER) $(CUBE_PATH)handle-cube-input.hpp $(CUBE_PATH)projection.hpp $(CUBE_PATH)read-cube-file.hpp
 cluster.o : src/cluster.cpp $(LSH_HEADERS)common.hpp
--- src/modules/kmeansPP/kmeansPP.cpp
+++ src/modules/kmeansPP/kmeansPP.cpp
@@ -14,40 +14,42 @@ vector<int*> previous_centroids;
 
 int getRandomNumber(int number) {
 	random_device generator;
-	uniform_int_distribution<int> distribution (0, number);
+	uniform_int_distribution<int> distribution (0, number - 1);
 	return distribution(generator);
 }
 
 // MAY NEED CHANGES!!!!!!!! D^2
-int nextInitialCentroidIndex(uint32_t number_of_images) {
-    //	Adding the rest of the points according to the probability
-	unsigned int total_distance = 0;
-	for (int i = 0; i < number_of_images ; i++)
-		total_distance += nearest_clusters[i].second;
+int* nextInitialCentroidCoords(uint32_t number_of_images) {
+    // Adding the rest of the points according to the probability
+	// unsigned int total_distance = 0;
+	// for (int i = 0; i < number_of_images ; i++)
+	// 	total_distance += nearest_clusters[i].second;
 
-	vector<unsigned int> v_i(number_of_images, 0);
-	for (int i = 0; i < number_of_images ; i++)
-		v_i[i] = (nearest_clusters[i].second / total_distance);
+	// vector<unsigned int> v_i(number_of_images, 0);
+	// for (int i = 0; i < number_of_images ; i++)
+	// 	v_i[i] = (nearest_clusters[i].second / total_distance);
 	
-    // Adding
-	for (int i = 1; i < number_of_images ; i++)
-		v_i[i] += v_i[i-1];
+    // // Adding
+	// for (int i = 1; i < number_of_images ; i++)
+	// 	v_i[i] += v_i[i-1];
 
-    // random number between [0, 1]
-	int random_num = getRandomNumber(2);
-	for (int i = 0; i < v_i.size() ; i++)
-		if (random_num < v_i[i])
-			return i;
+    // // random number between [0, 1]
+	// int random_num = getRandomNumber(2);
+	// for (int i = 0; i < v_i.size() ; i++)
+	// 	if (random_num < v_i[i])
+	// 		return i;
 
-    // take last v(i)
-	return v_i.size() - 1;
+    // // take last v(i)
+	// return v_i.size() - 1;
+
+
 }
 
-void getClosestCentroid (uint32_t index, vector<int> centroids, uint64_t d) {
+void getClosestCentroid (uint32_t index, vector<int*> centroids, uint64_t d) {
     int closest_cluster = -1;
     unsigned int min_distance = inf;
     for (unsigned int c = 0; c < centroids.size() ; c++) {
-        unsigned int distance = manhattanDistance(cluster_images[centroids[c]], cluster_images[index], d);
+        unsigned int distance = manhattanDistance(centroids[c], cluster_images[index], d);
         if (distance < min_distance) {
             min_distance = distance;
             closest_cluster = c;
@@ -60,29 +62,57 @@ void getClosestCentroid (uint32_t index, vector<int> centroids, uint64_t d) {
     nearest_clusters[index] = make_pair(closest_cluster, min_distance);
 }
 
-void updateNearClusters(vector<int> centroids, uint32_t number_of_images, uint64_t d) {
+// calculate distance for every image for each centroid and
+// select the smallest distance and save it in this cluster's centroid
+// This function should have three different bodies
+// the current body represent the Lloyd's, the other two will be the reverse LSH and reverse Cube
+void updateNearClusters(vector<int*> centroids, uint32_t number_of_images, uint64_t d) {
     for (uint32_t i = 0; i < number_of_images; i++) {
         getClosestCentroid(i, centroids, d);        
     }
 }
 
-void updateCentroids(vector<int> centroids, uint32_t number_of_images, uint64_t d) {
-    vector<int> size_of_cluster.resize(centroids.size());
-    vector<int*> new_centroids.resize(centroids.size());
+// k-medians, calculate the median in order to get next 
+// cluster's centroid coords
+void updateCentroids(vector<int*> centroids, uint32_t number_of_images, uint64_t d) {
+    vector<int> temp_array;
+    vector<int *> new_centroids;
+
+    temp_array.resize(d);
+    new_centroids.resize(centroids.size());
 
     for (unsigned int i=0; i<new_centroids.size(); i++) {
         new_centroids[i] = new int [d];
     }
-    for (uint32_t i = 0; i < number_of_images ; i++) {
-        size_of_cluster[nearest_clusters[i].first]++;
-        for(int j=0 ; j < d ; j++)
-            new_centroids[nearest_clusters[i].first][j] += input_data[i][j];
+
+    for (int c = 0; c < centroids.size(); c++) {
+        // for loop for every image in the cluster c
+        // call function that returns a vector with all the images in a given cluster, by centroid index
+        for (){
+
+            // make a for loop for every dimension of the d
+            for (int i = 0; i < d; i++) {
+                // calculate median value of all cluster_images in this cluster
+                temp_array[i] = ;
+            }
+            // calculate the median of this temp array and store it in new_centroids[c][i]
+            // in new_centroids[c] place the new coords
+
+            temp_array.clear();
+        }
     }
-    for (int i = 0; i < centroids.size() ; ++i)
-        if(freq[i])
-            for (int j = 0; j < centroids[i].size() ; ++j)
-                new_centroids[i][j] *= (1.0/(double)freq[i]);
-    cur_centroids_ = new_centroids;
+    // NOT TESTED
+    // for (uint32_t i = 0; i < number_of_images ; i++) {
+    //     size_of_cluster[nearest_clusters[i].first]++;
+    //     for(int j=0 ; j < d ; j++)
+    //         new_centroids[nearest_clusters[i].first][j] += input_data[i][j];
+    // }
+    // for (int i = 0; i < centroids.size() ; ++i)
+    //     if(freq[i])
+    //         for (int j = 0; j < centroids[i].size() ; ++j)
+    //             new_centroids[i][j] *= (1.0/(double)freq[i]);
+    // cur_centroids_ = new_centroids;
+
 }
 
 bool equalCentroids (uint64_t d) {
@@ -94,29 +124,39 @@ bool equalCentroids (uint64_t d) {
 }
 
 vector<vector<int> > kmeansPP (int K, uint32_t number_of_images, uint64_t d) {
+    // this is a vector of all centroids
+    // for each centroid store a vector of images assigned in this cluster
     vector<vector<int> > clusters(K);
     // Get a random centroid
-    int first_centroid = getRandomNumber(number_of_images);
+    int* first_centroid = new int [d];
+    for (int i = 0; i < d;i++){
+        // it will give random numbers for every dimension of the first centroid
+        // max number will be 255 and min 0
+        first_centroid[i] = getRandomNumber(256);
+    }
 
     initial_centroids.push_back(first_centroid);
-    
+    // it will have one position for each image
     nearest_clusters.resize(number_of_images);
 
     // first centroid was randomly selected already
+    // continue with Classic method (Lloyd's)
     for (int i = 1; i < K; i++) {
+        // 
         updateNearClusters(initial_centroids, number_of_images, d);
-        initial_centroids.push_back(nextInitialCentroidIndex(number_of_images));
+        initial_centroids.push_back(nextInitialCentroidCoords(number_of_images));
     }
 
     current_centroids = initial_centroids;
-    // continue with Classic method (Lloyd's)
     do {
         previous_centroids = current_centroids;
+        // 
         updateNearClusters(current_centroids, number_of_images, d);
-        updateCentroids(current_centroids);
+        updateCentroids(current_centroids, number_of_images, d);
 
     } while (!equalCentroids(d));
 
+    // finally 
     for (uint32_t i = 0; i < number_of_images;i++)
         clusters[nearest_clusters[i].first].push_back(i);
     return clusters;
